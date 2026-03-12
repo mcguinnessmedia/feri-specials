52c590b9c8611a3f350142580c5cfa25c50b41bf
ben <ben@mcguinnessmedia.com>
2026-03-12 16:12:53 -0400

    add necessary types

---
+++ src/lib/types.ts
@@ -0,0 +1,31 @@
+export type Area = 'fridge' | 'freezer';
+
+export interface SpecialsItem {
+  name: string;
+  price: string;
+  unlimited: boolean;
+  special: boolean;
+  locations: number[];
+}
+
+export interface SpecialsApiResponse {
+  lastUpdated: string;
+  marketOpen: boolean;
+  dateLabel: string;
+  footerLeft: string;
+  footerCenter: string;
+  fridge: SpecialsItem[];
+  freezer: SpecialsItem[];
+}
+
+export interface GroupedSpecialsSection {
+  key: string;
+  label: string;
+  locations: number[];
+  items: SpecialsItem[];
+}
+
+export interface GroupedSpecials {
+  fridge: GroupedSpecialsSection[];
+  freezer: GroupedSpecialsSection[];
+}
