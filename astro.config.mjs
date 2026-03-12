a8a8109c4121fd3c75f893107a0eda9756329d7b
ben <ben@mcguinnessmedia.com>
2026-03-12 16:38:02 -0400

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hopemarket.feedri.org',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
